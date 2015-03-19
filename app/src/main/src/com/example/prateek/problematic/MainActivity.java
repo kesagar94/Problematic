52e7b36f76e4e0c9f1552042e1dafad710a52e41
kesagar94 <kesagar94@gmail.com>
2015-03-20 01:51:47 +0530

    changes

---
+++ app/src/main/src/com/example/prateek/problematic/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.prateek.problematic;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+@TargetApi(Build.VERSION_CODES.ECLAIR)
+public class MainActivity extends Activity {
+    Button btn_login;
+    EditText login_email,login_password;
+    //TextView tv_login_status;
+    HttpPost httppost;
+    StringBuffer buffer;
+    HttpResponse response;
+    HttpClient httpclient;
+    List<NameValuePair> nameValuePairs;
+    ProgressDialog dialog = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) { //test
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = (Button)findViewById(R.id.btn_login);
+        login_email = (EditText)findViewById(R.id.login_email);
+        login_password= (EditText)findViewById(R.id.login_password);
+       // tv_login_status = (TextView)findViewById(R.id.tv_login_status);
+
+        btn_login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	setContentView(R.layout.activity_movie);
+                dialog = ProgressDialog.show(MainActivity.this, "",
+                        "Validating user...", true);
+                new Thread(new Runnable() {
+                    public void run() {
+                    	System.out.println("Yahoo");
+                        login();
+                    }
+                }).start();
+            }
+        });
+    }
+
+    void login(){
+        try{
+dialog.dismiss();
+        	System.out.println("Yahoo1");
+            httpclient=new DefaultHttpClient();
+            httppost= new HttpPost("http://technimatics.byethost11.com/login.php"); // make sure the url is correct.
+        	System.out.println("Yahoo2");
+            //add your data
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+        	System.out.println("Yahoo3");
+            // Always use the same variable name for posting i.e the android side variable name and php side variable name should be similar,
+            nameValuePairs.add(new BasicNameValuePair("login_email",login_email.getText().toString().trim()));  // $Edittext_value = $_POST['Edittext_value'];
+
+        	System.out.println("Yahoo4");
+        	nameValuePairs.add(new BasicNameValuePair("login_password",login_password.getText().toString().trim()));
+        	System.out.println("Yahoo5");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        	System.out.println("Yahoo6");
+            //Execute HTTP Post Request
+            response=httpclient.execute(httppost);
+        	System.out.println("Yahoo7");
+            // edited by James from coderzheaven.. from here....
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        	System.out.println("Yahoo8");
+            final String response = httpclient.execute(httppost, responseHandler);
+            System.out.println("Response : " + response);
+        dialog.setTitle(response);
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    //tv_login_status.setText("Response from PHP : " + response);
+                    dialog.dismiss();
+                }
+            });
+
+            if(response.contains("success")){
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this,"Login Success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                startActivity(new Intent(MainActivity.this,UserProfile.class));
+                overridePendingTransition(R.anim.top_out, R.anim.bottom_in);
+            }else{
+                showAlert();
+            }
+
+        }catch(Exception e){
+            dialog.dismiss();
+            System.out.println("Exception : " + e.getMessage());
+        }
+    }
+    public void showAlert(){
+        MainActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Login Error.");
+                builder.setMessage("User not Found.")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
