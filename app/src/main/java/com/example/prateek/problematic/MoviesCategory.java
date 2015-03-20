2b19b6c5e4ca66eb196376bf9b91b12962d1ba40
Prateek <joshi.prateek15@gmail.com>
2015-03-20 16:51:22 -0700

    new design of movie category and linked movie btn

---
+++ app/src/main/java/com/example/prateek/problematic/MoviesCategory.java
@@ -0,0 +1,18 @@
+package com.example.prateek.problematic;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by prateek on 3/20/2015.
+ */
+public class MoviesCategory extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+
+    }
+}
