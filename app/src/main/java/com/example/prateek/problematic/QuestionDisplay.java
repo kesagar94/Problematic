1237891c203854c44657ff980fd975bbe9d7a45a
Prateek <joshi.prateek15@gmail.com>
2015-03-21 01:34:12 -0700

    added question page for all categories

---
+++ app/src/main/java/com/example/prateek/problematic/QuestionDisplay.java
@@ -0,0 +1,17 @@
+package com.example.prateek.problematic;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by prateek on 3/21/2015.
+ */
+public class QuestionDisplay  extends ActionBarActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+
+    }
+}
